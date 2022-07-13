391f22f87f17faf820c0afcb2d851ceffb2f387f
ForHemer <foreste.hemerson@gmail.com>
2022-07-12 23:16:10 -0400

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  namespace :v1, defaults: {format: 'json'} do
-    get 'greetings', to: 'greetings#index'
-  end
-  # Defines the root path route ("/")
+   # Defines the root path route ("/")
   root "static#index"
+  namespace :v1, defaults: {format: 'json'} do
+    get 'greeting', to: 'greetings#index'
+  end
+ 
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
